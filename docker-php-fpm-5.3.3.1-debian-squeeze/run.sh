a21a56fea0080da3e450dbc8c806631913523fba
root <root@ns227630.comptoir.co>
2020-03-31 18:07:44 +0000

    repty needs PTRACE cap. on recent docker

--- docker-php-fpm-5.3.3.1-debian-squeeze/run.sh
+++ docker-php-fpm-5.3.3.1-debian-squeeze/run.sh
@@ -12,5 +12,6 @@ docker run \
   --volume /dev/log:/dev/log \
   --name php-fpm-5.3.3.1-debian-squeeze \
   --log-driver syslog --log-opt tag=php-fpm-5.3.3.1-debian-squeeze \
+  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
   php-fpm-5.3.3.1-debian-squeeze \
   /bin/sh -c '/usr/sbin/php5-fpm --fpm-config /etc/php5/fpm/php-fpm.conf && sleep 2 && reptyr -s `ps aux | grep /usr/sbin/php5-fpm | grep "^root " | grep -v grep | awk "{ print \\$2 }" | sort -n | head -n 1`'
